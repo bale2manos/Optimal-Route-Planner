36bab24df04ebdf1a7ac8915035a94b9959868f6
Javier P <100472252@alumnos.uc3m.es>
2023-11-30 16:02:08 +0100

    main updated

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import csv
+import sys
+from mapa import Mapa
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+    if len(sys.argv) != 3:
+        print("Uso: python ASTARTraslados.py <path mapa.csv> <num-h>")
+        sys.exit(1)
+
+    archivo_mapa = sys.argv[1]
+    num_heuristica = int(sys.argv[2])
+
+    # Validar que num_heuristica sea 1 o 2
+    if num_heuristica not in {1, 2}:
+        print("El parámetro num-h debe ser 1 o 2.")
+        sys.exit(1)
+
+    mapa = Mapa(archivo_mapa)
+    camino = mapa.a_estrella(num_heuristica)
+
+    if camino:
+        # Escribir el camino en un archivo
+        with open('camino_solucion.txt', 'w') as archivo_salida:
+            for nodo in camino:
+                celda = nodo.mapa.get_celda(nodo.ambulancia.celdaX, nodo.ambulancia.celdaY)
+                linea = f"({celda.fila},{celda.columna}):{celda.tipo}:{nodo.ambulancia.energia_left}\n"
+                archivo_salida.write(linea)
+        print("Camino encontrado y escrito en 'camino_solucion.txt'")
+    else:
+        print("No se encontró un camino.")
+
+if __name__ == "__main__":
+    main()
+
